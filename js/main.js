5539f3dd8364ab390623909ca2ef3fab1b032aeb
Juan Soto <jpsoto121@gmail.com>
2023-02-25 18:29:05 -0500

    Final details to code

--- js/main.js
+++ js/main.js
@@ -16,179 +16,156 @@ const partialResult = document.querySelector('#partial-result');
 
 
 function endGame(userScore, cpuScore) {
+  const spanClose = document.createElement('span');
+  spanClose.classList.add('close');
 
-    const spanClose = document.createElement('span');
-    spanClose.classList.add('close');
+  const h1 = document.createElement('h1');
 
-    const h1 = document.createElement('h1');
+  const p = document.createElement('p');
 
-    const p = document.createElement('p');
+  let h1Text; 
+  let text;
 
-    let h1Text; 
-    let text;
+  if (userScore === 10) {
+    h1.classList.add('text-win');
+    h1Text = document.createTextNode('VICTORY')
+    text = document.createTextNode('End of the game. You reached 10 wins! Go ahead and win another match.')
+  } else if (cpuScore === 10) {
+    h1.classList.add('text-lose');
+    h1Text = document.createTextNode('DEFEAT')
+    text = document.createTextNode('End of the game. CPU reached 10 wins! Try again.')
+  }
 
-    if (userScore === 10) {
-        h1.classList.add('text-win');
-        h1Text = document.createTextNode('VICTORY')
-        text = document.createTextNode('End of the game. You reached 10 wins! Go ahead and win another match.')
-    } else if (cpuScore === 10) {
-        h1.classList.add('text-lose');
-        h1Text = document.createTextNode('DEFEAT')
-        text = document.createTextNode('End of the game. CPU reached 10 wins! Try again.')
-    }
-
-    h1.append(h1Text);
-    p.append(text);
-    
-    result.innerHTML = '';
-    result.append(spanClose, h1, p);
-    modal.style.display = 'block';
-    
-    console.log('entró a la funcion')
+  h1.append(h1Text);
+  p.append(text);
+  
+  result.innerHTML = '';
+  result.append(spanClose, h1, p);
+  modal.style.display = 'block';
 }
 
 
 function getCpuChoice() {
-    const options = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random() * options.length);
-    return options[random];
+  const options = ['rock', 'paper', 'scissors'];
+  const random = Math.floor(Math.random() * options.length);
+  return options[random];
 }
+
 
 function win(userChoice, cpuChoice) {
-    userScore++;
-    userScore_span.innerHTML = userScore;
-    cpuScore_span.innerHTML = cpuScore;
+  userScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
 
-    if (userScore === 10) {
-        endGame(userScore, cpuScore);
-    }
+  if (userScore === 10) {
+    endGame(userScore, cpuScore);
+  }
     
-    partialResult.innerHTML = '';
+  partialResult.innerHTML = '';
 
-    const span = document.createElement('span');
-    span.classList.add('text-win');
-    spanText = document.createTextNode('Win! ');
-    span.append(spanText);
+  const span = document.createElement('span');
+  span.classList.add('text-win');
+  spanText = document.createTextNode('Win! ');
+  span.append(spanText);
 
-    const text = document.createTextNode(`CPU chose ${cpuChoice}, luck is on your side this time!`)
+  const text = document.createTextNode(`CPU chose ${cpuChoice}, luck is on your side this time!`)
 
-    partialResult.append(span, text);
-    /* result.innerHTML = `<span class="close"></span> <h1 class="text-win">YOU WON</h1> <p>CPU chose <strong>${cpuChoice}</strong>, luck is on your side this time!</p>`;
-    modal.style.display = 'block'; */
-
-    // console.log('You won, luck is on your side this time!');
-    // console.log(`Player 1: ${userChoice}\nCPU: ${cpuChoice}`);
+  partialResult.append(span, text);
 }
+
 
 function lose(userChoice, cpuChoice) {
-    cpuScore++;
-    userScore_span.innerHTML = userScore;
-    cpuScore_span.innerHTML = cpuScore;
+  cpuScore++;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
 
-    if (cpuScore === 10) {
-        endGame(userScore, cpuScore);
-    }
+  if (cpuScore === 10) {
+    endGame(userScore, cpuScore);
+  }
 
-    partialResult.innerHTML = '';
+  partialResult.innerHTML = '';
 
-    const span = document.createElement('span');
-    span.classList.add('text-lose');
-    spanText = document.createTextNode('Lose! ');
-    span.append(spanText);
+  const span = document.createElement('span');
+  span.classList.add('text-lose');
+  spanText = document.createTextNode('Lose! ');
+  span.append(spanText);
 
-    const text = document.createTextNode(`CPU chose ${cpuChoice}, maybe you'll be lucky next time!`)
+  const text = document.createTextNode(`CPU chose ${cpuChoice}, maybe you'll be lucky next time!`)
 
-    partialResult.append(span, text);
-    /* result.innerHTML = `<span class="close"></span> <h1 class="text-lose">YOU LOST</h1> <p>CPU chose <strong>${cpuChoice}</strong>, maybe you will be lucky next time!</p>`;
-    modal.style.display = 'block'; */
-
-    // console.log('You lost :C: Maybe you will be lucky next time!');
-    // console.log(`Player 1: ${userChoice}\nCPU: ${cpuChoice}`);
+  partialResult.append(span, text);
 }
 
-function draw(cpuChoice) {
-    /* userScore_span.innerHTML = userScore;
-    cpuScore_span.innerHTML = cpuScore; */
-    /* result.innerHTML = `<span class="close"></span> <h1 class="text-draw">IT'S A DRAW</h1> <p>You both chose <strong>${cpuChoice}</strong>, try again!</p>`;
-    modal.style.display = 'block'; */
 
+function draw(cpuChoice) {
     partialResult.innerHTML = '';
 
     const text = document.createTextNode(`It's a tie. Try again!`)
-
     partialResult.append(text);
-
-
-    console.log('entró a draw');
-
-    // console.log(`It's a tie. Try again!`);
-    // console.log(`Player 1: ${userChoice}\nCPU: ${cpuChoice}`);
 }
 
+
 function play(userChoice) {
-    const cpuChoice = getCpuChoice();
-    switch (userChoice + cpuChoice) {
-        case 'rockscissors':
-        case 'paperrock':
-        case 'scissorspaper':
-            win(userChoice, cpuChoice);
-            break;
-        case 'rockpaper':
-        case 'paperscissors':
-        case 'scissorsrock':
-            lose(userChoice, cpuChoice);
-            break;
-        case 'rockrock':
-        case 'paperpaper':
-        case 'scissorsscissors':
-            draw(cpuChoice);
-    }
+  const cpuChoice = getCpuChoice();
+  switch (userChoice + cpuChoice) {
+    case 'rockscissors':
+    case 'paperrock':
+    case 'scissorspaper':
+      win(userChoice, cpuChoice);
+      break;
+    case 'rockpaper':
+    case 'paperscissors':
+    case 'scissorsrock':
+      lose(userChoice, cpuChoice);
+      break;
+    case 'rockrock':
+    case 'paperpaper':
+    case 'scissorsscissors':
+      draw(cpuChoice);
+  }
 }
 
 
 function main() {
-    rock_div.addEventListener('click', function() {
-        play('rock');
-    })
+  rock_div.addEventListener('click', function() {
+    play('rock');
+  })
 
-    paper_div.addEventListener('click', function() {
-        play('paper');
-    })
+  paper_div.addEventListener('click', function() {
+    play('paper');
+  })
 
-    scissors_div.addEventListener('click', function() {
-        play('scissors');
-    })
+  scissors_div.addEventListener('click', function() {
+    play('scissors');
+  })
 }
 
 
 function clearModal(e) {
-    closeBtn = document.querySelector('.close');
+  closeBtn = document.querySelector('.close');
 
-    if (e.target == modal) {
-        modal.style.display = "none"
-        restartGame()
-    }
-    else if (closeBtn) {
-        closeBtn.addEventListener('click', function() {
-            modal.style.display = "none"
-            restartGame()
-        })
-    }
-
-    
+  if (e.target == modal) {
+    modal.style.display = "none"
+    restartGame()
+  } else if (closeBtn) {
+    closeBtn.addEventListener('click', function() {
+      modal.style.display = "none"
+      restartGame()
+    })
+  }
 }
 
 
 function restartGame() {
-    userScore = 0;
-    cpuScore = 0;
-    userScore_span.innerHTML = userScore;
-    cpuScore_span.innerHTML = cpuScore;
-    partialResult.innerHTML = '';
+  userScore = 0;
+  cpuScore = 0;
+  userScore_span.innerHTML = userScore;
+  cpuScore_span.innerHTML = cpuScore;
+  partialResult.innerHTML = '';
 }
 
 
 restart.addEventListener('click', restartGame);
 window.addEventListener('click', clearModal);
+
 
 main();
